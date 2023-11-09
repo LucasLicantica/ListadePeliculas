88b0423bb44e2a5fdeba6ee8d4e1c699344690b3
LucasLicantica <Lucasmaxi05@gmail.com>
2023-11-09 12:04:26 -0300

    Agregue un archivo

--- peliculas/language.py
+++ peliculas/language.py
@@ -11,10 +11,9 @@ bp = Blueprint('language', __name__, url_prefix="/language/")
 @bp.route('/')
 def index():
     db = get_db()
-    languajes = db.execute(
-        """SELECT l.name AS lenguaje, l.name AS lenguaje 
-         FROM film l JOIN language l ON l.language_id = f.language_id
-         ORDER BY l.film_id DESC"""
+    languages = db.execute(
+        """SELECT l.name, l.language_id FROM language l 
+         ORDER BY l.language_id DESC"""
     ).fetchall()
-    return render_template('language/index.html', language=languajes)
+    return render_template('language/index.html', languages=languages)
 
